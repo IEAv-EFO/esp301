e1653b2393281f24e988f16dde9bb081672db501
Gabriel Nunes <nunesgfs@gmail.com>
2023-03-15 17:18:32 -0300

    programming mode working

--- script/make_linearity.jl
+++ script/make_linearity.jl
@@ -6,19 +6,22 @@ touch(file_name)
 file = open(file_name, "w")
 
 # position limits 
-pos_lim = 45
+pos_lim = 90
 time_lim = 5
 
 # angular velocities are in deg/s
-first_vel = 5
+first_vel = 15
 last_vel = 40
-step_vel = 5
+step_vel = 2
 
 angular_vel = first_vel:step_vel:last_vel
 
+
+# start programming
+write(file, "1xx\n1ep\n")
+
 # send home
-send_home = "1ac100; 1va40; 1pa0; 1ws1000\n"
-write(file, send_home)
+write(file, "1mo; 1ac80; 1va40; 1pr-45; wt3000\n")
 
 for speed in angular_vel
 
@@ -27,10 +30,12 @@ for speed in angular_vel
         pos = pos_lim
     end
 
-    command = "1va$speed; 1pa+$pos; 1ws; 1pa-$pos; 1ws500\n"
+    command = "1va$speed; 1pr+$pos; 1ws; 1pr-$pos; 1ws1000\n"
     write(file, command)
 end
 
-write(file, send_home)
+# stop programming
+write(file,  "1ac80; 1va40; 1pr45; wt3000\n")
+write(file, "qp\n1ex\n")
 
 close(file)
