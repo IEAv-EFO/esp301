3a33af23402185909a3171ee4ffba2f084d495f6
Gabriel Nunes <40604481+gabriellnuness@users.noreply.github.com>
2023-03-15 13:13:52 -0300

    script in julia to create linearity

--- script/make_linearity.jl
+++ script/make_linearity.jl
@@ -1,5 +1,10 @@
 # code to generate script text file to use in ESP301 GUI
 
+# file creationg
+file_name = "linearity_by_julia.txt"
+touch(file_name)
+file = open(file_name, "w")
+
 # position limits 
 pos_lim = 45
 time_lim = 5
@@ -11,8 +16,21 @@ step_vel = 5
 
 angular_vel = first_vel:step_vel:last_vel
 
+# send home
+send_home = "1ac100; 1va40; 1pa0; 1ws1000\n"
+write(file, send_home)
+
 for speed in angular_vel
 
-    
-    println("1va$speed; 1pa+$pos; 1ws; 1pa-$pos; 1ws500")
+    pos = time_lim*speed
+    if pos  >= pos_lim
+        pos = pos_lim
+    end
+
+    command = "1va$speed; 1pa+$pos; 1ws; 1pa-$pos; 1ws500\n"
+    write(file, command)
 end
+
+write(file, send_home)
+
+close(file)
